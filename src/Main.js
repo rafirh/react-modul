46ffe7d99aca5b97cc659425362027a0d35b3a72
Rafi Rahman <rafirahmann18@gmail.com>
2022-03-17 12:01:07 +0700

    first commit

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Gallery from "./pages/Gallery"
+ 
+class Main extends React.Component{
+    render(){
+        return (
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/gallery" component={Gallery} />
+            </Switch>
+        )
+    }
+}
+export default Main;
